475d558c470566f737f2b19ebda93a1f73701060
Pava Pai <pava@uw.edu>
2020-03-08 18:37:57 -0700

    socket

---
+++ src/main/java/com/rsvps/RSVPApplication.java
@@ -0,0 +1,30 @@
+package com.rsvps;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+
+@SpringBootApplication
+public class RSVPApplication {
+    private static final String MEETUP_RSVPS_ENDPOINT = "ws://stream.meetup.com/2/rsvps";
+
+    public static void main(String[] args) {
+        SpringApplication.run(RSVPApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner initializeConnection(
+            RsvpsWebSocketHandler rsvpsWebSocketHandler) {
+// Hand shake protocol to establish connection with kafka
+        return args -> {
+            WebSocketClient rsvpsSocketClient = new StandardWebSocketClient();
+
+            rsvpsSocketClient.doHandshake(
+                    rsvpsWebSocketHandler, MEETUP_RSVPS_ENDPOINT);
+        };
+    }
+
+}
---
+++ src/main/java/com/rsvps/RsvpsWebSocketHandler.java
@@ -0,0 +1,23 @@
+package com.rsvps;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+@Component
+class RsvpsWebSocketHandler extends AbstractWebSocketHandler {
+
+    private static final Logger logger =
+            Logger.getLogger(RsvpsWebSocketHandler.class.getName());
+
+    @Override
+    public void handleMessage(WebSocketSession session,
+                              WebSocketMessage<?> message) {
+      System.out.println(message.getPayload());
+
+
+    }
+}
